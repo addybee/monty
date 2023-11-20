f349742c8e922bce81dc441d9e58df893ba5201b
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2023-11-19 16:22:02 -0800

    nop task

--- monty.h
+++ monty.h
@@ -64,6 +64,8 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 char **get_argv(char *command);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_strdup(char *str);
